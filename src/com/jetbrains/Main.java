69b0d4ebcb5db53fd22a50550c27bd53ac763d74
Nathalie Flores <21250@student.dorset-college.ie>
2020-10-20 07:30:17 -0600

    Add second commit - product object

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,8 +1,30 @@
 package com.jetbrains;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private List<Product> productList = new ArrayList<>();
+    private double totalCartValue;
+
+    public int getProductCount() {
+        return productList.size();
+    }
+
+    public void addProduct(Product product) {
+        //implement
+    }
+
+    public double getTotalCartValue() {
+        //implement
+        return totalCartValue;
+    }
+
+    public Product getProductByName(String name) {
+        if (productList.size() > 0) {
+            productList.stream().filter(product -> product.getName().equals(name)).findFirst().get();
+        }
+        return null;
     }
 }
---
+++ src/com/jetbrains/Product.java
@@ -0,0 +1,38 @@
+package com.jetbrains;
+
+public class Product {
+
+    private String name;
+    private int quantity;
+    private double totalPrice;
+
+    public Product(String name, int quantity, double totalPrice) {
+        this.name = name;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
